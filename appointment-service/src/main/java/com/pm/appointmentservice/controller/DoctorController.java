609d8e52b8253c6aa5f3c52798bd55ec0a065414
Yash Falke <yashfalke77@gmail.com>
2026-03-07 20:00:02 +0530

    adding loggers

--- appointment-service/src/main/java/com/pm/appointmentservice/controller/DoctorController.java
+++ appointment-service/src/main/java/com/pm/appointmentservice/controller/DoctorController.java
@@ -2,6 +2,8 @@ package com.pm.appointmentservice.controller;
 
 import com.pm.appointmentservice.dto.DoctorResponseDTO;
 import com.pm.appointmentservice.service.DoctorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,13 @@ import java.util.List;
 @RequestMapping("/doctors")
 public class DoctorController {
 
+    private static final Logger log = LoggerFactory.getLogger(DoctorController.class);
     @Autowired
     DoctorService doctorService;
 
     @GetMapping
     public ResponseEntity<List<DoctorResponseDTO>> getDoctors(){
+        log.info("Inside Doctors API");
         return ResponseEntity.ok().body(doctorService.getDoctors());
     }
 }
--- appointment-service/src/main/java/com/pm/appointmentservice/service/DoctorService.java
+++ appointment-service/src/main/java/com/pm/appointmentservice/service/DoctorService.java
@@ -6,17 +6,21 @@ import com.pm.appointmentservice.mapper.DoctorDetailMapper;
 import com.pm.appointmentservice.model.DoctorDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Service
 public class DoctorService {
 
+    private static final Logger log = LoggerFactory.getLogger(DoctorService.class);
     @Autowired
     DoctorDetailDao doctorDetailDao;
 
     public List<DoctorResponseDTO> getDoctors(){
         List<DoctorDetail>  doctorDetails = doctorDetailDao.findAll();
+        log.info("Inside service {}", doctorDetails.size());
         return doctorDetails.stream().map(DoctorDetailMapper::toResponseDTO).toList();
     }
 }
